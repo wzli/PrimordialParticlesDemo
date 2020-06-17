ff675c64ceb88cec5a9b5075bf7aaab693d08b6b
WenZheng Li <me@wenzheng.li>
2020-06-18 07:33:56 +0900

    filtered out of viewBox particles from svg

--- include/display.hpp
+++ include/display.hpp
@@ -8,7 +8,7 @@ struct Display {
     std::string svg_bg_color = "black";
     float particle_radius = 0.2f;
 
-    const char* assignParticleColor(const Particles::Particle& particle) {
+    static const char* assignParticleColor(const Particles::Particle& particle) {
         size_t total_neighbors = particle.left_neighbors + particle.right_neighbors;
         if (total_neighbors > 35) {
             return "yellow";
@@ -23,7 +23,7 @@ struct Display {
         }
     }
 
-    void drawParticlesSvg(std::stringstream& ss, const Particles& particles) {
+    void drawParticlesSvg(std::stringstream& ss, const Particles& particles) const {
         auto r = particles.getConfig().simulation_radius * M_SQRT1_2;
         auto origin = particles.getConfig().simulation_origin;
         ss << "<svg xmlns=\"http://www.w3.org/2000/svg\" ";
@@ -34,6 +34,12 @@ struct Display {
            << 2 * r << "\" >\r\n";
         ss << "<g>\r\n";
         for (const auto& particle : particles.getParticles()) {
+            if (particle.second.position.x() < origin.x() - r ||
+                    particle.second.position.x() > origin.x() + r ||
+                    particle.second.position.y() < origin.y() - r ||
+                    particle.second.position.y() > origin.y() + r) {
+                continue;
+            }
             ss << "<circle ";
             ss << "r=\"" << particle_radius << "\" ";
             ss << "cx=\"" << particle.second.position.x() << "\" ";
