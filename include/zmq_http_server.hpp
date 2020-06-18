8a8cc83417ac2e26f1cdb0dfa9c3093dfee43d13
WenZheng Li <me@wenzheng.li>
2020-06-19 04:34:26 +0900

    better zmq http_server api

--- include/zmq_http_server.hpp
+++ include/zmq_http_server.hpp
@@ -17,13 +17,15 @@ public:
         _request_handlers[path] = std::move(request_handler);
     }
 
-    void poll(int timeout = -1);
+    void poll(int receive_timeout = -1, int send_timeout = 1000);
 
 private:
     static void parsePath(char* path, size_t len, const char* request_data);
 
-    void sendResponse(zmq::message_t request_handle, zmq::message_t response);
-    void sendResponse404(zmq::message_t request_handle);
+    void sendResponse(zmq::message_t& request_handle, const void* buf, int len);
+    void sendResponse(zmq::message_t& request_handle, zmq::message_t& response);
+
+    void sendMessage(zmq::message_t& msg) { _http_socket.send(msg, zmq::send_flags::sndmore); }
 
     zmq::context_t _zmq_ctx;
     zmq::socket_t _http_socket;
--- src/zmq_http_server.cpp
+++ src/zmq_http_server.cpp
@@ -1,7 +1,14 @@
 #include <zmq_http_server.hpp>
 
-void ZmqHttpServer::poll(int timeout) {
-    _http_socket.set(zmq::sockopt::rcvtimeo, timeout);
+static constexpr char HTTP_404[] =
+        "HTTP/1.1 404 Not Found\r\n"
+        "Content-Type: text/plain\r\n"
+        "\r\n"
+        "404 Page Not Found";
+
+void ZmqHttpServer::poll(int receive_timeout, int send_timeout) {
+    _http_socket.set(zmq::sockopt::rcvtimeo, receive_timeout);
+    _http_socket.set(zmq::sockopt::sndtimeo, send_timeout);
     zmq::message_t request_handle;
     zmq::recv_result_t recv_result;
     recv_result = _http_socket.recv(request_handle);
@@ -17,11 +24,11 @@ void ZmqHttpServer::poll(int timeout) {
     parsePath(path, sizeof(path), static_cast<const char*>(request.data()));
     auto request_handler = _request_handlers.find(path);
     if (request_handler == _request_handlers.end()) {
-        sendResponse404(std::move(request_handle));
+        sendResponse(request_handle, HTTP_404, sizeof(HTTP_404));
         return;
     }
     auto response = request_handler->second(std::move(request));
-    sendResponse(std::move(request_handle), std::move(response));
+    sendResponse(request_handle, response);
 }
 
 void ZmqHttpServer::parsePath(char* path, size_t len, const char* request_data) {
@@ -36,20 +43,15 @@ void ZmqHttpServer::parsePath(char* path, size_t len, const char* request_data) 
     *c = 0;
 }
 
-void ZmqHttpServer::sendResponse(zmq::message_t request_handle, zmq::message_t response) {
-    zmq::message_t handle_view(request_handle.data(), request_handle.size(), nullptr, nullptr);
-    _http_socket.send(handle_view, zmq::send_flags::sndmore);
-    _http_socket.send(response, zmq::send_flags::sndmore);
-    _http_socket.send(request_handle, zmq::send_flags::sndmore);
-    _http_socket.send(request_handle, zmq::send_flags::none);
+void ZmqHttpServer::sendResponse(zmq::message_t& request_handle, zmq::message_t& response) {
+    zmq::message_t response_handle(request_handle.data(), request_handle.size(), nullptr, nullptr);
+    sendMessage(response_handle);
+    sendMessage(response);
+    sendMessage(request_handle);
+    sendMessage(request_handle);
 }
 
-void ZmqHttpServer::sendResponse404(zmq::message_t request_handle) {
-    static constexpr char HTTP_404[] =
-            "HTTP/1.1 404 Not Found\r\n"
-            "Content-Type: text/plain\r\n"
-            "\r\n"
-            "404 Page Not Found";
-    zmq::message_t error_response(const_cast<char*>(HTTP_404), sizeof(HTTP_404));
-    sendResponse(std::move(request_handle), std::move(error_response));
+void ZmqHttpServer::sendResponse(zmq::message_t& request_handle, const void* buf, int len) {
+    zmq::message_t response(const_cast<void*>(buf), len, nullptr, nullptr);
+    sendResponse(request_handle, response);
 }
