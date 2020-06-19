42576c2426956e0768646fd78adbd9637eb23789
WenZheng Li <me@wenzheng.li>
2020-06-19 12:53:25 +0900

    added mesh node thread

--- src/sim_node.cpp
+++ src/sim_node.cpp
@@ -9,8 +9,23 @@
 
 #include <unistd.h>
 #include <iostream>
+#include <thread>
 
 static constexpr int UPDATE_INTERVAL = 20;  // ms
+
+static vsm::MeshNode::Config mesh_config{
+        vsm::msecs(500),  // peer update interval
+        {
+                "node_name",              // name
+                "udp://127.0.0.1:11511",  // address
+                {0, 0},                   // coordinates
+                10,                       // connection_degree
+                200,                      // lookup size
+                0,                        // rank decay
+        },
+        std::make_shared<vsm::ZmqTransport>("udp://*:11511"),  // transport
+        std::make_shared<vsm::Logger>(),                       // logger
+};
 
 static constexpr char INDEX_RESPONSE[] =
         "HTTP/1.1 200 OK\r\n"
@@ -39,6 +54,7 @@ static std::stringstream compress(std::stringstream& in) {
 }
 
 int main(int argc, char* argv[]) {
+    // parse arguments
     const char* http_port = "8000";
     const char* mesh_port = "11511";
     const char* initial_peers = nullptr;
@@ -54,11 +70,11 @@ int main(int argc, char* argv[]) {
             case 'b':
                 mesh_port = optarg;
                 break;
-            case '?':  // unknown option...
+            case '?':
                 puts(HELP_STRING);
                 std::cerr << "Unknown option: '" << static_cast<char>(optopt) << "'" << std::endl;
                 return -1;
-            case ':':  // unknown option...
+            case ':':
                 puts(HELP_STRING);
                 std::cerr << "Option: '" << static_cast<char>(optopt) << "' requires argument"
                           << std::endl;
@@ -71,32 +87,25 @@ int main(int argc, char* argv[]) {
     }
     printf("i %s p %s b %s a %s\n", initial_peers, http_port, mesh_port, argv[optind]);
 
-    vsm::MeshNode::Config mesh_config{
-            vsm::msecs(1),  // peer update interval
-            {
-                    "self_name",              // name
-                    "udp://127.0.0.1:11511",  // address
-                    {100, 100},               // coordinates
-                    4,                        // connection_degree
-                    200,                      // lookup size
-                    0,                        // rank decay
-            },
-            std::make_shared<vsm::ZmqTransport>("udp://*:11511"),  // transport
-            std::make_shared<vsm::Logger>(),                       // logger
-    };
-    vsm::MeshNode mesh_node(mesh_config);
-
+    // particle sim objects
     Display display;
     Particles::Config sim_config;
     Particles particles(sim_config);
+
+    // network objects
+    vsm::MeshNode mesh_node(mesh_config);
     ZmqHttpServer http_server(http_port);
+
+    // particle sim update timer
     http_server.addTimer(UPDATE_INTERVAL, [&particles](int) { particles.update(); });
 
+    // default page
     http_server.addRequestHandler("/", [](zmq::message_t) {
         return zmq::message_t(
                 const_cast<char*>(INDEX_RESPONSE), sizeof(INDEX_RESPONSE), nullptr, nullptr);
     });
 
+    // spawn particle on click
     http_server.addRequestHandler("/spawn", [&particles](zmq::message_t msg) {
         float x, y;
         if (sscanf(static_cast<const char*>(msg.data()), "GET /spawn?x=%f&y=%f HTTP", &x, &y) ==
@@ -113,6 +122,7 @@ int main(int argc, char* argv[]) {
                 const_cast<char*>(response_data), sizeof(response_data), nullptr, nullptr);
     });
 
+    // generate particle display
     http_server.addRequestHandler("/particles", [&particles, &display](zmq::message_t) {
         std::stringstream svg_stream;
         display.drawParticlesSvg(svg_stream, particles);
@@ -120,6 +130,7 @@ int main(int argc, char* argv[]) {
         return zmq::message_t(response.c_str(), response.size());
     });
 
+    // generate network display
     http_server.addRequestHandler("/network", [&mesh_node, &display](zmq::message_t) {
         std::stringstream svg_stream;
         display.drawNetworkSvg(svg_stream, mesh_node);
@@ -127,6 +138,15 @@ int main(int argc, char* argv[]) {
         return zmq::message_t(response.c_str(), response.size());
     });
 
+    // worker thread runs mesh network
+    std::thread mesh_thread([&mesh_node]() {
+        while (1) {
+            mesh_node.getTransport().poll(vsm::msecs(-1));
+        }
+    });
+    mesh_thread.detach();
+
+    // main thread runs http server and particle sim
     while (1) {
         try {
             http_server.poll();
