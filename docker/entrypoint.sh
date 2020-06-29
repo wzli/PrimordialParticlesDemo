889eddd8348184fee0f20a0b43a0fdf728a21aa0
WenZheng Li <me@wenzheng.li>
2020-06-29 18:00:01 +0900

    made entry point script

---
+++ docker/entrypoint.sh
@@ -0,0 +1,16 @@
+ADDRESS=${ADDRESS:-$(hostname -I)}
+EXTERNAL_ADDRESS=${EXTERNAL_ADDRESS:-$(hostname)}
+HTTP_PORT=${HTTP_PORT:-80}
+MESH_PORT=${MESH_PORT:-11511}
+BOOTSTRAP_PEER=${BOOTSTRAP_PEER:-${ADDRESS%.*}.2:$MESH_PORT}
+
+SIM_RADIUS=${SIM_RADIUS:-20}
+SIM_DENSITY=${SIM_DENSITY:-0.04}
+SIM_INTERVAL=${SIM_INTERVAL:-50}
+CONNECTION_DEGREE=${CONNECTOIN_DEGREE:-4}
+COORDS=${COORDS:-$(spiral $((${ADDRESS##*.} - 2)) $SIM_RADIUS)}
+
+sim_node --http-port $HTTP_PORT --mesh-port $MESH_PORT --address $ADDRESS \
+  --name $EXTERNAL_ADDRESS --bootstrap-peer $BOOTSTRAP_PEER $COORDS \
+  --sim-radius $SIM_RADIUS --sim-density $SIM_DENSITY --sim-interval $SIM_INTERVAL \
+  --connection-degree $CONNECTION_DEGREE || sleep infinity
